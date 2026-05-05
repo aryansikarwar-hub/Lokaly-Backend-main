1171df47c0d84be2dd65776df1ae92e074889f6d
aryansikarwar-hub <aryansinghsikarwar518@gmail.com>
2026-05-06 01:40:57 +0530

    fix auth route

--- src/routes/coHostRoutes.js
+++ src/routes/coHostRoutes.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
+const router = require('express').Router();
 const ctrl = require('../controllers/coHostController');
+const { requireAuth } = require('../middleware/auth');
 
 // Public routes
 router.get('/', ctrl.getAllCoHosts);
@@ -9,13 +8,13 @@ router.get('/categories/stats', ctrl.getCategoryStats);
 router.get('/:id', ctrl.getCoHostById);
 
 // Protected routes
-router.post('/', protect, ctrl.applyAsCoHost);
-router.put('/:id', protect, ctrl.updateCoHost);
-router.patch('/:id/availability', protect, ctrl.toggleAvailability);
-router.delete('/:id', protect, ctrl.deleteCoHost);
+router.post('/', requireAuth, ctrl.applyAsCoHost);
+router.put('/:id', requireAuth, ctrl.updateCoHost);
+router.patch('/:id/availability', requireAuth, ctrl.toggleAvailability);
+router.delete('/:id', requireAuth, ctrl.deleteCoHost);
 
 // Booking routes
-router.post('/:id/book', protect, ctrl.bookCoHost);
-router.get('/:id/bookings', protect, ctrl.getCoHostBookings);
+router.post('/:id/book', requireAuth, ctrl.bookCoHost);
+router.get('/:id/bookings', requireAuth, ctrl.getCoHostBookings);
 
 module.exports = router;
